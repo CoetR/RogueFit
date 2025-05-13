6c928d0f4e27ce4207769de53b85526c2bd85235
Ruben Coetzee <coetzeeruben24@gmail.com>
2025-05-13 16:01:40 +0200

    Add ViewModels folder with ShopViewModel model

---
+++ ViewModels/ShopViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RogueFit.Models;
+
+namespace RogueFit.ViewModels
+{
+    public class ShopViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<SubCategory> SubCategories { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
