e70610c4ec5263f6eff949d2f74bfae6e9d1d2cb
Ruben Coetzee <coetzeeruben24@gmail.com>
2025-05-13 15:20:01 +0200

    Add SubCategory class and update DbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -21,5 +21,6 @@ namespace RogueFit.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
     }
 }
---
+++ Models/SubCategory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RogueFit.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
