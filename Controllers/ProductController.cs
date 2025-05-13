9ad76ba78b277901b2dc22d1575eb36aea6bfae8
Ruben Coetzee <coetzeeruben24@gmail.com>
2025-05-13 15:51:52 +0200

    Add ProductController with DbContext and Index method

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RogueFit.Data;
+
+namespace RogueFit.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ProductController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var products = dbContext.Products.ToList();
+            return View(products);
+        }
+    }
+}
