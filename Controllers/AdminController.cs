a724d3e3bcab0d6d0392991c48d91b4ec2f46d2b
Ruben Coetzee <coetzeeruben24@gmail.com>
2025-05-14 12:08:18 +0200

    Add AdminController and basic Admin dashboard page with nav to manage Products and Categories and Styling page for Dashboard

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RogueFit.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
