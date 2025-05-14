762ab466537c1eac4c8ce69304a03d036f9e5c31
Ruben Coetzee <coetzeeruben24@gmail.com>
2025-05-14 12:41:10 +0200

    Added ProductAdminController and admin manage listing page with Edit and Delete actions, wired up navigation for Create action and add styling page

---
+++ Controllers/ProductAdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RogueFit.Data;
+
+namespace RogueFit.Controllers
+{
+    public class ProductAdminController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ProductAdminController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await dbContext.Products
+                .Include(p => p.Tag)
+                .ThenInclude(t => t.SubCategory)
+                .ThenInclude(sc => sc.Category)
+                .ToListAsync();
+
+            return View(products);
+        }
+    }
+}
